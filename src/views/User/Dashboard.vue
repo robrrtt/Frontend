ca1e18027f85f74b157831c4f3bc0d39b38dd28a
robrrtt <janipin0610@gmail.com>
2024-05-15 04:15:59 +0800

    Column and row

--- src/views/User/Dashboard.vue
+++ src/views/User/Dashboard.vue
@@ -1,36 +1,31 @@
 <template>
-  <div>
-    <h1 class="">DashboardPage</h1>
-    <button
-      @click="logout"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute"
-    >
-      Logout
-    </button>
+  <div id="app">
+    <Header />
   </div>
-
-  <Sidebar />
-  <div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-red-700">
-      <div class="grid gap-4 mb-4">
-        <div class="overflow-x-auto">
-          <table class="min-w-full border-collapse table-auto">
-            <thead>
-              <tr class="bg-gray-200">
-                <th class="border border-gray-300 px-4 py-2">Name</th>
-                <th class="border border-gray-300 px-4 py-2">Author</th>
-                <th class="border border-gray-300 px-4 py-2">Publish Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <!-- Loop through each book in the books array -->
-              <tr v-for="book in books.data" :key="book.id" class="bg-white">
-                <td class="border border-gray-300 px-4 py-2">{{ book.name }}</td>
-                <td class="border border-gray-300 px-4 py-2">{{ book.author }}</td>
-                <td class="border border-gray-300 px-4 py-2">{{ book.publish_date }}</td>
-              </tr>
-            </tbody>
-          </table>
+  <div class="grid grid-column-2 grid-flow-col gap-2 h-screen m-2">
+    <div class="h-full max-h-full bg-lime-500 mt-20"></div>
+    <div class="h-full max-h-full bg-red-500 mt-20 col-span-2 md:col-span-87">
+      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-red-700">
+        <div class="grid gap-4 mb-4">
+          <div class="overflow-x-auto mt-">
+            <table class="min-w-full border-collapse table-auto">
+              <thead>
+                <tr class="bg-gray-200">
+                  <th class="border border-gray-300 px-4 py-2">Name</th>
+                  <th class="border border-gray-300 px-4 py-2">Author</th>
+                  <th class="border border-gray-300 px-4 py-2">Publish Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                Loop through each book in the books array
+                <tr v-for="book in books.data" :key="book.id" class="bg-white">
+                  <td class="border border-gray-300 px-4 py-2">{{ book.name }}</td>
+                  <td class="border border-gray-300 px-4 py-2">{{ book.author }}</td>
+                  <td class="border border-gray-300 px-4 py-2">{{ book.publish_date }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -38,30 +33,11 @@
 </template>
 
 <script setup>
-import Sidebar from '../components/Sidebar.vue'
+import Header from '../components/Header.vue'
+import Sidebar from '../components/Header.vue'
+
 import axios from 'axios'
-import { useRouter } from 'vue-router'
 import { ref, onMounted } from 'vue'
-
-const router = useRouter()
-
-const logout = async () => {
-  try {
-    await axios.post(
-      'http://127.0.0.1:8080/api/auth/logout',
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-      }
-    )
-    localStorage.removeItem('token')
-    router.push({ name: 'loginpage' })
-  } catch (error) {
-    console.error('Logout error:', error.response.data)
-  }
-}
 
 const books = ref({ data: [] }) // Initialize as an object with a 'data' property
 
